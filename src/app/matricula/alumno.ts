1e9f18227864fe8b09e8c28ca530bad9bad28a3c
jyarango <jyarango.13@gmail.com>
2024-06-15 00:48:00 -0500

    8-oktodomenospagos

---
+++ src/app/matricula/alumno.ts
@@ -0,0 +1,15 @@
+export class Alumno{
+    constructor (
+        public id?: string,
+        public nombres?: string,
+        public apellidos?: string,
+        public fecNac?:Date,
+        public direccion?: string,
+        public correo?: string,
+        public telefono?: string,
+    
+    ){}
+}
+
+//Estructura de Alumno (id, nombres, apellidos, fecha de nacimiento, 
+//dirección, correo, teléfono).  Cantidad para la lista: 15.
--- src/app/matricula/curso.ts
+++ src/app/matricula/curso.ts
@@ -2,7 +2,13 @@ export class Curso{
     constructor (
         public id?: string,
         public nombre?: string,
+        public horas?: number,
         public creditos?: number,
+        public categoria?: string,
+
     
     ){}
 }
+
+//horas [de 1 a 24], créditos[de 0 a 5], 
+//categoría[software, gestión, otros]). 
