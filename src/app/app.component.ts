ba4dae01225e55f69e4bc55cbe09c94e150d32d8
jyarango <jyarango.13@gmail.com>
2024-06-14 21:35:46 -0500

    3-utilsevecorrecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CabeceraAppComponent } from './utiles/cabecera-app/cabecera-app.component';
 import { PieAppComponent } from './utiles/pie-app/pie-app.component';
 import { CarruselAppComponent } from './utiles/carrusel-app/carrusel-app.component';
+import { FormularioMatriculaComponent } from './matricula/formulario-matricula/formulario-matricula.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CabeceraAppComponent,PieAppComponent,CarruselAppComponent],
+  imports: [RouterOutlet,CabeceraAppComponent,PieAppComponent,CarruselAppComponent,FormularioMatriculaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'cjavapc-app';
--- src/app/utiles/utiles.module.ts
+++ src/app/utiles/utiles.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CabeceraAppComponent } from './cabecera-app/cabecera-app.component';
-import { CarruselAppComponent } from './carrusel-app/carrusel-app.component';
-import { PieAppComponent } from './pie-app/pie-app.component';
+
 
 
 
