095ea886a538384f95c1574f89a806b701789243
jyarango <jyarango.13@gmail.com>
2024-06-15 15:22:59 -0500

    10-listaalumnosok

--- src/app/gestion/lista-alumnos/lista-alumnos.component.ts
+++ src/app/gestion/lista-alumnos/lista-alumnos.component.ts
@@ -1,12 +1,14 @@
+
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-alumnos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './lista-alumnos.component.html',
-  styleUrl: './lista-alumnos.component.css'
+  styleUrls: ['./lista-alumnos.component.css']
 })
 export class ListaAlumnosComponent implements OnInit {
   alumnos = [
@@ -15,15 +17,45 @@ export class ListaAlumnosComponent implements OnInit {
     // Agrega más alumnos según sea necesario
   ];
 
+  showForm = false;
+
+  newAlumno = {
+    id: 0,
+    nombres: '',
+    apellidos: '',
+    fechaNacimiento: '',
+    direccion: '',
+    correo: '',
+    telefono: ''
+  };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  toggleForm(): void {
+    this.showForm = !this.showForm;
   }
 
   addAlumno(): void {
-    // Lógica para agregar un nuevo alumno
+    const newId = this.alumnos.length > 0 ? Math.max(...this.alumnos.map(alumno => alumno.id)) + 1 : 1;
+    this.newAlumno.id = newId;
+    this.alumnos.push({ ...this.newAlumno });
+    this.resetForm();
+  }
+
+  resetForm(): void {
+    this.newAlumno = {
+      id: 0,
+      nombres: '',
+      apellidos: '',
+      fechaNacimiento: '',
+      direccion: '',
+      correo: '',
+      telefono: ''
+    };
+    this.showForm = false;
   }
 }
-
 
 
