a092761de092b039b73d0885918f02f1cb39ac6d
jyarango <jyarango.13@gmail.com>
2024-06-14 21:53:13 -0500

    4-funcionarutaCursos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListaCursosComponent } from './gestion/lista-cursos/lista-cursos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'/home',pathMatch:'full' },
+    { path: 'cursos', component: ListaCursosComponent }
+];
